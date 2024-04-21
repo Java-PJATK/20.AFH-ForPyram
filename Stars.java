d22b79c1d711d82a6231a02d37ff725f6475df42
piecucci <piecucci@gmail.com>
2024-04-21 23:17:36 +0200

    Add files via upload

---
+++ Stars.java
@@ -0,0 +1,20 @@
+// AFH-ForPyram/Stars.java
+ 
+import java.util.Scanner;
+
+public class Stars {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a positive odd number: ");
+        int n = scan.nextInt();
+        scan.close();
+
+        for (int len=1, sp=n/2; len <= n; len+=2, --sp) {
+            for (int i = 0; i < sp; ++i)
+                System.out.print(" ");
+            for (int i = 0; i < len; ++i)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
